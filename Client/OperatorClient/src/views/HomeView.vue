dec8393813f2875f2cab65270a80f4e0befc850a
Nick Häcker <nick.athaeck@googlemail.com>
2024-07-29 20:01:52 +0200

    add 120 to selection

--- Client/OperatorClient/src/views/HomeView.vue
+++ Client/OperatorClient/src/views/HomeView.vue
@@ -232,7 +232,7 @@ function Calibrate() {
               <div class="action-field-element">
                 <span>Anzahl PCD Frames</span>
                 <v-select label="Anzahl der PCD Frames" :disabled="!isCalibratorInIdle" v-model="pcdFrames"
-                  :items="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]"></v-select>
+                  :items="[1, 2, 3, 4, 120, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]"></v-select>
               </div>
               <div class="action-field-element">
                 <span>Erstelle Init NPZ</span>
